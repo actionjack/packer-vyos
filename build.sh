51b6c2f69847741129c37d1bf723112ad559b02c
svigneux <svigneux@kronostechnologies.com>
2014-03-16 15:14:14 -0400

    courtesy clean.sh helper script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packer build -only=virtualbox-iso vyos-1.0.2-i386.json
 packer build -only=virtualbox-iso vyos-1.0.2-amd64.json
+packer build -only=virtualbox-iso vyos-1.0.2-i386.json
 packer build -only=virtualbox-iso vyatta-core-6.6R1-amd64.json
 packer build -only=virtualbox-iso vyatta-core-6.6R1-i386.json
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+vagrant box remove vyos-1.0.2-amd64
+vagrant box remove vyos-1.0.2-i386
+vagrant box remove vyatta-core-6.6R1-amd64
+vagrant box remove vyatta-core-6.6R1-i386
